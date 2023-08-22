b5f4965f121b47fcda2d3624720de28a3ee9c4e0
EmaBR23 <79341113+EmaBR23@users.noreply.github.com>
2023-08-22 00:22:25 -0600

    Mas casos prueba fifo_flops_tb.sv

--- Simulation/fifo_flops_tb.sv
+++ Simulation/fifo_flops_tb.sv
@@ -50,11 +50,13 @@ module fifo_flops_tb();
     
     task prueba();
         if (full == 1) begin
+            // Si el FIFO está lleno, vamos al ciclo 3 para manejarlo
             ciclo = 3;
         end
         
         case(ciclo)
             0:begin
+                // Configuración inicial del sistema
                 rst = 1'b0;
                 push = 1'b0;
                 pop = 1'b0;
@@ -63,6 +65,7 @@ module fifo_flops_tb();
             end
             
             1:begin
+                // Activamos el reset para limpiar el FIFO
                 rst = 1'b1;
                 push = 1'b0;
                 pop = 1'b0;
@@ -71,35 +74,144 @@ module fifo_flops_tb();
             end
             
             2:begin
+                // Alternamos entre push y no-push
                 rst = 1'b1;
                 push = ~push;
                 pop = 1'b0;
                 Din = dato;
                 
-                if (push==1)begin
-                    $display("at $g pushed data: %g count %g", $time,Din,fifo_flops_DUT.count);
+                if (push == 1) begin
+                    // Realizamos un push y mostramos el dato y el contador
+                    $display("at $g pushed data: %g count %g", $time, Din, fifo_flops_DUT.count);
                     dato = dato + 1;
                 end
                 
-                if (dato==8) ciclo = 3;
+                if (dato == 16) begin
+                    // Cuando el FIFO está lleno, vamos al ciclo 3
+                    ciclo = 3;
+                    $display("FIFO is full");
+                end
             end
-                
+            
             3:begin
-                rst = 1'b1;
+                // Llenado del FIFO completado, volvemos a la configuración inicial
+                rst = 1'b0;
                 push = 1'b0;
+                pop = 1'b0;
+                Din = 8'b00;
+                ciclo = 4;
+            end
+            
+            4:begin
+                // Activamos el pop para extraer un dato del FIFO
+                pop = 1'b1;
+                $display("at $g popping data: %g count %g", $time, Dout, fifo_flops_DUT.count);
+                ciclo = 5;
+            end
+            
+            5:begin
+                // Volvemos a la configuración inicial
+                rst = 1'b0;
+                push = 1'b0;
+                pop = 1'b0;
+                Din = 8'b00;
+                ciclo = 6;
+            end
+            
+            6:begin
+                // Operaciones de push y pop al mismo tiempo
+                rst = 1'b1;
+                push = ~push;
                 pop = ~pop;
                 Din = dato;
                 
-                if (push==1)begin
-                    $display("at $g pushed data: %g count %g", $time,Din,fifo_flops_DUT.count);
-                    
+                if (push == 1) begin
+                    // Realizamos un push y mostramos el dato y el contador
+                    $display("at $g pushed data: %g count %g", $time, Din, fifo_flops_DUT.count);
+                    dato = dato + 1;
                 end
                 
+                if (pop == 1) begin
+                    // Realizamos un pop y mostramos el dato y el contador
+                    $display("at $g popping data: %g count %g", $time, Dout, fifo_flops_DUT.count);
+                end
+                
+                if (dato == 8) begin
+                    // Cuando el FIFO está medio lleno, vamos al ciclo 7
+                    ciclo = 7;
+                    $display("FIFO is half-full");
+                end
             end
-                
-                
             
+            7:begin
+                // El FIFO está medio lleno, volvemos a la configuración inicial
+                rst = 1'b0;
+                push = 1'b0;
+                pop = 1'b0;
+                Din = 8'b00;
+                ciclo = 8;
+            end
             
+            8:begin
+                // Activamos el reset nuevamente
+                rst = 1'b1;
+                push = 1'b0;
+                pop = 1'b0;
+                Din = 8'b00;
+                ciclo = 9;
+            end
+            
+            9:begin
+                // Intentamos hacer un push cuando el FIFO ya está lleno
+                rst = 1'b1;
+                push = 1'b1;
+                pop = 1'b0;
+                Din = dato;
+                
+                if (push == 1) begin
+                    // Realizamos un push y mostramos el dato y el contador
+                    $display("at $g pushed data: %g count %g", $time, Din, fifo_flops_DUT.count);
+                    dato = dato + 1;
+                end
+                
+                if (full == 1) begin
+                    // Cuando el FIFO está lleno, vamos al ciclo 10
+                    ciclo = 10;
+                    $display("FIFO is full");
+                end
+            end
+            
+            10:begin
+                // El FIFO está lleno, volvemos a la configuración inicial
+                rst = 1'b0;
+                push = 1'b0;
+                pop = 1'b0;
+                Din = 8'b00;
+                ciclo = 11;
+            end
+            
+            11:begin
+                // Volvemos a la configuración inicial
+                rst = 1'b1;
+                push = 1'b0;
+                pop = 1'b0;
+                Din = 8'b00;
+                ciclo = 12;
+            end
+            
+            12:begin
+                // Continuamos las operaciones de push y pop
+                rst = 1'b1;
+                push = ~push;
+                pop = 1'b0;
+                Din = dato;
+                
+                if (push == 1) begin
+                    // Realizamos un push y mostramos el dato y el contador
+                    $display("at $g pushed data: %g count %g", $time, Din, fifo_flops_DUT.count);
+                    dato = dato + 1;
+                end
+            end
         endcase
     endtask
 
