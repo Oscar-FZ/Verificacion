aaef23cf58b994b0f19d8d5e01af83b7a29de76e
EmaBR23 <79341113+EmaBR23@users.noreply.github.com>
2023-08-23 15:31:57 -0600

    comment mux_parametrizable.sv

--- Sources/mux_parametrizable.sv
+++ Sources/mux_parametrizable.sv
@@ -1,4 +1,17 @@
-`timescale 1ns / 1ps
+// Módulo: mux_parametrizable
+// Descripción: Este módulo implementa un multiplexor parametrizable con una profundidad
+//              y ancho de datos configurables. El módulo toma como entrada una señal
+//              de selección (mux_sel_i) que indica el índice del dato de entrada a
+//              seleccionar, y un conjunto de datos de entrada (mux_data_i) de longitud
+//              depth. El módulo proporciona una señal de salida (mux_data_o) que es el
+//              dato de entrada seleccionado.
+// Parámetros:
+//   - depth: Profundidad del multiplexor (por defecto 16 entradas).
+//   - bits: Ancho de datos del multiplexor (por defecto 8 bits).
+// Puertos:
+//   - mux_sel_i: Señal de selección de entrada.
+//   - mux_data_i: Conjunto de datos de entrada.
+//   - mux_data_o: Dato de salida seleccionado.
 
 module mux_parametrizable #(parameter depth = 16, parameter bits = 8)
     (
@@ -8,24 +21,28 @@ module mux_parametrizable #(parameter depth = 16, parameter bits = 8)
         output reg [bits-1:0] mux_data_o
     );
     
+    // Registros auxiliares para el multiplexor
     reg [bits-1:0] aux_mux [depth-1:0];        
     reg [bits-1:0] aux_mux_or [depth-2:0];
     
+    // Generar lógica del multiplexor
     genvar i;
     generate 
-        for (i = 0; i<depth; i = i+1) begin
-            assign aux_mux[i] = (mux_sel_i == i + 1) ? mux_data_i[i] : {bits{1'b0}};
+        for (i = 0; i < depth; i = i + 1) begin
+            assign aux_mux[i] = (mux_sel_i == i + 1) ? mux_data_i[i] : {bits{1'b0}}; // Seleccionar el dato correcto o ceros.
         end
     endgenerate
     
     generate
-        for (i = 0; i<depth-2; i = i+1) begin
-            assign aux_mux_or[i] = aux_mux[i] | aux_mux_or[i+1];
+        for (i = 0; i < depth-2; i = i + 1) begin
+            assign aux_mux_or[i] = aux_mux[i] | aux_mux_or[i+1]; // Realizar la operación OR para los datos seleccionados.
         end
     endgenerate
     
-    always@(*)begin
-        aux_mux_or[depth-2] = aux_mux[depth-1] | aux_mux[depth-2];
-        mux_data_o = aux_mux_or[0];
+    // Asignar el dato seleccionado a la salida
+    always @(*) begin
+        aux_mux_or[depth-2] = aux_mux[depth-1] | aux_mux[depth-2]; // Calcular el último OR intermedio.
+        mux_data_o = aux_mux_or[0]; // La salida es el resultado del primer OR intermedio.
     end
 endmodule
+
