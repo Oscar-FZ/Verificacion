a72459cae3e955de2a4c62adf6ddceb886976f43
EmaBR23 <79341113+EmaBR23@users.noreply.github.com>
2023-08-23 15:26:56 -0600

    comment fifo_control.sv

--- Sources/fifo_control.sv
+++ Sources/fifo_control.sv
@@ -1,4 +1,18 @@
-`timescale 1ns / 1ps
+// Módulo: fifo_control
+// Descripción: Este módulo controla el contador de elementos en un FIFO.
+//              El módulo toma como entradas las señales de reloj (clk_i), reinicio (rst_i),
+//              operación de inserción (push) y operación de extracción (pop). El módulo
+//              genera la señal de salida "count" que representa la cantidad actual de elementos
+//              en el FIFO. El contador se actualiza en función de las operaciones de inserción
+//              y extracción.
+// Parámetros:
+//   - depth: Profundidad máxima del FIFO (por defecto 16 elementos)
+// Puertos:
+//   - clk_i: Señal de reloj de entrada.
+//   - rst_i: Señal de reinicio de entrada (activo en flanco negativo).
+//   - push: Señal de operación de inserción en el FIFO.
+//   - pop: Señal de operación de extracción del FIFO.
+//   - count: Salida que indica la cantidad actual de elementos en el FIFO.
 
 module fifo_control #(parameter depth = 16)
     (
@@ -10,46 +24,42 @@ module fifo_control #(parameter depth = 16)
         output reg [$clog2(depth):0] count
     );
     
+    // Definición de los estados posibles del módulo
     typedef enum
-        {
+    {
         no_push_no_pop,
         only_pop,
         only_push,
         push_pop
-        } state_t;
-        
+    } state_t;
     
-        
+    // Procesamiento asincrónico para actualizar el contador "count"
     always_ff @(posedge clk_i, negedge rst_i)
         if (!rst_i)
-            count <= 0;
+            count <= 0; // En caso de reinicio, el contador se restablece a cero.
         else begin
-            unique case ({push,pop}) 
-                no_push_no_pop: count <= count;
+            unique case ({push, pop})
+                no_push_no_pop: count <= count; // No hay operaciones de inserción ni extracción, no cambia el contador.
                 
                 only_pop: begin
                     if (count == 0) begin
-                        count <= 0;
+                        count <= 0; // Si el contador es cero, permanece igual.
                     end
-                    
                     else begin
-                        count = count - 1;
+                        count = count - 1; // Si hay una operación de extracción, disminuye el contador.
                     end
                 end
                 
                 only_push: begin
                     if (count == depth) begin
-                        count <= count;
+                        count <= count; // Si el contador ya alcanzó la profundidad máxima, permanece igual.
                     end
-                    
                     else begin
-                        count = count + 1;
+                        count = count + 1; // Si hay una operación de inserción, aumenta el contador.
                     end
                 end
                 
-                push_pop: count <= count;
+                push_pop: count <= count; // Si hay tanto operación de inserción como extracción, el contador no cambia.
             endcase
         end
-            
-               
 endmodule
